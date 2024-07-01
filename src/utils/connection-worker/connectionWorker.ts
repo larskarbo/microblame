8fca3f426db340945013c49efb289b608b1e1804
Lars Karbo <10865165+larskarbo@users.noreply.github.com>
2024-07-01 12:24:35 +0200

    temp disable connection-worker

--- src/utils/connection-worker/connectionWorker.ts
+++ src/utils/connection-worker/connectionWorker.ts
@@ -17,71 +17,77 @@ const start = async () => {
     );
 
     for (const instance of instances) {
-      const pg = await getPostgresJsInstance({ instance });
+      try {
+        const pg = await getPostgresJsInstance({ instance });
 
-      const snapshot = await pg<
-        {
-          datid: number;
-          datname: string;
-          pid: number;
-          usesysid: number;
-          usename: string;
-          application_name: string;
-          client_addr: string;
-          client_hostname: string;
-          client_port: number;
-          backend_start: Date;
-          xact_start: Date;
-          query_start: Date;
-          state_change: Date;
-          wait_event_type: string;
-          wait_event: string;
-          state: string;
-          backend_xid: number;
-          backend_xmin: number;
-          query: string;
-        }[]
-      >`
-        SELECT
-          datid,
-          datname,
-          pid,
-          usesysid,
-          usename,
-          application_name,
-          client_addr,
-          client_hostname,
-          client_port,
-          backend_start,
-          xact_start,
-          query_start,
-          state_change,
-          wait_event_type,
-          wait_event,
-          state,
-          backend_xid,
-          backend_xmin,
-          query
-        FROM pg_stat_activity;
-      `;
+        const snapshot = await pg<
+          {
+            datid: number;
+            datname: string;
+            pid: number;
+            usesysid: number;
+            usename: string;
+            application_name: string;
+            client_addr: string;
+            client_hostname: string;
+            client_port: number;
+            backend_start: Date;
+            xact_start: Date;
+            query_start: Date;
+            state_change: Date;
+            wait_event_type: string;
+            wait_event: string;
+            state: string;
+            backend_xid: number;
+            backend_xmin: number;
+            query: string;
+          }[]
+        >`
+					SELECT
+						datid,
+						datname,
+						pid,
+						usesysid,
+						usename,
+						application_name,
+						client_addr,
+						client_hostname,
+						client_port,
+						backend_start,
+						xact_start,
+						query_start,
+						state_change,
+						wait_event_type,
+						wait_event,
+						state,
+						backend_xid,
+						backend_xmin,
+						query
+					FROM pg_stat_activity;
+				`;
 
-      console.log(
-        `Inserting ${snapshot.length} rows for instance ${instance.uuid}`
-      );
+        console.log(
+          `Inserting ${snapshot.length} rows for instance ${instance.uuid}`
+        );
 
-      await clickhouseClient.insert({
-        table: "pg_stat_activity_snapshot",
-        values: snapshot.map((row) => ({
-          instance_uuid: instance.uuid,
-          ...row,
-          waiting:
-            row.wait_event_type !== null || row.wait_event !== null
-              ? "true"
-              : "false",
-          snapshot_time: new Date(),
-        })),
-        format: "JSONEachRow",
-      });
+        await clickhouseClient.insert({
+          table: "pg_stat_activity_snapshot",
+          values: snapshot.map((row) => ({
+            instance_uuid: instance.uuid,
+            ...row,
+            waiting:
+              row.wait_event_type !== null || row.wait_event !== null
+                ? "true"
+                : "false",
+            snapshot_time: new Date(),
+          })),
+          format: "JSONEachRow",
+        });
+      } catch (e) {
+        console.log(`Failed to get snapshot for instance ${instance.uuid}`, {
+          error: e,
+        });
+      }
     }
   };
 
